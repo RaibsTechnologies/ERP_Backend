9c2f3e077e6160d4cb4d6cd7bfa5ba39c9b19615
Guhan <guhanntamil@gmail.com>
2025-08-22 18:54:00 +0530

    finish logic in login and register

--- router/authRouter.js
+++ router/authRouter.js
@@ -2,13 +2,14 @@ const express = require('express');
 const {registerUser , login} = require('../controller/authController');
 const upload = require('../middleware/fileUpload');
 const auth = require('../middleware/auth');
-const {getUserDetails} = require('../controller/authController');
+const {getUserDetails ,uploadProfilePhoto} = require('../controller/authController');
 const router = express.Router();// Creating a new router instance
 
 
 router.post('/register', upload.single("profile"), registerUser )
 router.post('/login', login);
 router.get('/me' , auth , getUserDetails);
+router.post('/upload-profile', auth, upload.single("profile"), uploadProfilePhoto);
 
  
 module.exports = router; // Exporting the router for use in other modules
